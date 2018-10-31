276941b5abead39d386fe6c38407e3f9e6f618f1
Hary Rodriguez <imu24r@gmail.com>
2018-10-31 17:40:19 -0600

    Create extractor.py

---
+++ extractor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+#dic for users you want to check
+my_users = {}
+#print just the key to extract the usernames as the user is set as the key value
+for key, value in my_users.items():
+	print (key)
